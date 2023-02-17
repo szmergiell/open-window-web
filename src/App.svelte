e7326d4a9bd05579f6831c10108bb21e19b3f1c0
szmergiell <szmergiel@pm.me>
2023-02-17 20:04:55 +0100

    start working on open window component

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
-  import Counter from "./lib/Counter.svelte";
+    import OpenWindow from "./lib/OpenWindow.svelte";
 </script>
 
 <main>
-  <h1>Should you open window?</h1>
-  <div class="card">
-    <Counter />
-  </div>
+    <article>
+        <h1>Should you open window?</h1>
+        <p>
+            This little tool answers a question whether one should open windows
+            in order to decrease indoor humidity.
+        </p>
+        <p>
+            Question is answered by comparing indoor and outdoor dew points
+            calculated based on indoor/outdoor temperature and relative humidity
+            measurements.
+        </p>
+    </article>
+    <div class="card">
+        <OpenWindow />
+    </div>
 </main>
 
 <style>
---
+++ src/lib/OpenWindow.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+    let indoorTemperature: number = 0;
+    let indoorRelativeHumidity: number = 0;
+
+    let outdoorTemperature: number = 0;
+    let outdoorRelativeHumidity: number = 0;
+</script>
+
+<form>
+    <label>
+        Indoor temperature [°C]:
+        <input
+            type="number"
+            name="indoor-temperature"
+            value={indoorTemperature}
+            min="-100"
+            max="100"
+        />
+    </label>
+    <label>
+        Indoor relative humidity [%]:
+        <input
+            type="number"
+            name="indoor-relative-humidity"
+            value={indoorRelativeHumidity}
+            min="0"
+            max="100"
+        />
+    </label>
+
+    <label>
+        Outdoor temperature [°C]:
+        <input
+            type="number"
+            name="outdoor-temperature"
+            value={outdoorTemperature}
+            min="-100"
+            max="100"
+        />
+    </label>
+    <label>
+        Outdoor relative humidity [%]:
+        <input
+            type="number"
+            name="outdoor-relative-humidity"
+            value={outdoorRelativeHumidity}
+            min="0"
+            max="100"
+        />
+    </label>
+    <input type="submit" value="Submit" />
+</form>
+
+<style>
+    form {
+        display: flex;
+        flex-flow: column nowrap;
+        gap: 1rem;
+    }
+
+    label {
+        display: flex;
+        flex-flow: column nowrap;
+        gap: 0.5rem;
+        color: var(--green2);
+    }
+
+    input[type="submit"] {
+        margin-top: 1rem;
+        background-color: var(--green2);
+        color: var(--bg-dark);
+    }
+</style>
